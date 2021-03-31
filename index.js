5d966f422a2977c87c657f88847932b0be943adc
adb <info@adb.sh>
2021-03-31 21:42:23 +0200

    add configProxy and update config.json

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 import {DynDnsBot} from './DynDnsBot.js';
-import {loadData} from './jsonDataStore.js';
+import {loadData, storeData} from './jsonDataStore.js';
 
-let bot = new DynDnsBot({config: loadData('config.json')});
+let configPath = 'config.json';
+let config = loadData(configPath);
+
+let configProxy = new Proxy(config, {
+  set: (target, key, value) => {
+    console.log(`config changed: ${key} set from ${target[key]} to ${value}`);
+    target[key] = value;
+    storeData(config, configPath);
+    return true;
+  }
+});
+let bot = new DynDnsBot({config: configProxy});
 bot.setInterval();
